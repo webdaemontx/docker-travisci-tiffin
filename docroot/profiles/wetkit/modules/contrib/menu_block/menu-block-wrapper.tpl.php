7533b1398a5cfd724721c59e0e726843d9caf6f1
Jose Tavares <jose.tavares@ellucian.com>
2016-09-23 11:03:46 -0400

    updates to footer

--- docroot/profiles/wetkit/modules/contrib/menu_block/menu-block-wrapper.tpl.php
+++ docroot/profiles/wetkit/modules/contrib/menu_block/menu-block-wrapper.tpl.php
@@ -19,13 +19,5 @@
  */
 ?>
 <div class="<?php print $classes; ?>">
-    <div id="tiffin_logo" class="footer-logo">
-        <img alt="WxT Logo" src="http://tiffin.dev.dd:8083/sites/default/files/tiffin-logo_0.png">
-    </div>
-    <div id="footer-address">
-        <p> 155 Miami St | Tiffin, OH 44883<br/>
-        Phone: (800) 968-6446<br/>
-        <span id="copyright">&copy;2016 Tiffin University</span></p>
-    </div>
     <?php print render($content); ?>
 </div>
