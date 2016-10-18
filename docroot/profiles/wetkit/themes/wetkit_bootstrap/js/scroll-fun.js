f8df93a170703cd2a0cda012a2b2f6f8aaf1d0f6
Jose Tavares <jose.tavares@ellucian.com>
2016-10-18 18:23:22 -0400

    sticky homepage submenu

--- docroot/profiles/wetkit/themes/wetkit_bootstrap/js/scroll-fun.js
+++ docroot/profiles/wetkit/themes/wetkit_bootstrap/js/scroll-fun.js
@@ -4,8 +4,8 @@
 jQuery(function(){
 
     jQuery(window).scroll(function() {
-        if (jQuery(this).scrollTop() >= 90) {
-            jQuery('#mini-panel-homepage_submenu').addClass('sticky-bottom').fadeIn();
+        if (jQuery(this).scrollTop() >= 190) {
+            jQuery('#mini-panel-homepage_submenu').addClass('sticky-bottom');
         }
         else {
             jQuery('#mini-panel-homepage_submenu').removeClass('sticky-bottom');
@@ -14,3 +14,43 @@ jQuery(function(){
 
 
 });
+
+
+jQuery(document).ready(function () {
+
+
+
+    var youtu = jQuery("#you-and-tu").offset();
+    /* console.log(youtu.top) */
+
+
+    jQuery(window).scroll(function () {
+
+
+
+        var youtu = jQuery("#you-and-tu").offset();
+        var posventana = jQuery(window).scrollTop();
+
+
+        /* console.log("Window"+ jQuery(window).scrollTop());*/
+
+        if(youtu.top < posventana + 700){
+
+            /* WORKS! */
+
+            jQuery('#mini-panel-homepage_submenu').removeClass('sticky-bottom');
+
+        }
+    });
+
+});
+
+/*
+
+*/
+
+
+
+
+
+
