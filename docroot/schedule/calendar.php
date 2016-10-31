4294c494e79b8fa54c938b51a2d7fcf2a8459e7c
Sean Hanford <sean.hanford@ellucian.com>
2016-10-31 12:40:27 -0500

    Added custom PHP script for Scheduling Calendar in schedule directory off of docroot.

---
+++ docroot/schedule/calendar.php
@@ -0,0 +1,138 @@
+<?php
+
+header("Cache-Control: no-store, no-cache, must-revalidate, max-age=0");
+header("Cache-Control: post-check=0, pre-check=0", false);
+header("Pragma: no-cache");
+
+define('DRUPAL_ROOT', '/var/www/html/tiffin/docroot');
+require_once DRUPAL_ROOT . '/includes/bootstrap.inc';
+drupal_bootstrap(DRUPAL_BOOTSTRAP_FULL);
+
+include('php/connect.php'); 
+include('classes/class_calendar.php');
+
+$calendar = new booking_diary($link);
+
+if(isset($_GET['month'])) $month = $_GET['month']; else $month = date("m");
+if(isset($_GET['year'])) $year = $_GET['year']; else $year = date("Y");
+if(isset($_GET['day'])) $day = $_GET['day']; else $day = 0;
+
+// Unix Timestamp of the date a user has clicked on
+$selected_date = mktime(0, 0, 0, $month, 01, $year); 
+
+// Unix Timestamp of the previous month which is used to give the back arrow the correct month and year 
+$back = strtotime("-1 month", $selected_date); 
+
+// Unix Timestamp of the next month which is used to give the forward arrow the correct month and year 
+$forward = strtotime("+1 month", $selected_date);
+
+?>
+
+<!DOCTYPE HTML PUBLIC "-//W3C//DTD HTML 4.01 Transitional//EN" "http://www.w3.org/TR/html4/loose.dtd">
+<html>
+<head>
+
+<meta http-equiv="Content-Type" content="text/html; charset=utf-8">
+<title>Calendar</title>
+<link href="style.css" rel="stylesheet" type="text/css">
+
+<link href="http://fonts.googleapis.com/css?family=Droid+Serif" rel="stylesheet" type="text/css">
+<link href="http://fonts.googleapis.com/css?family=Droid+Sans" rel="stylesheet" type="text/css">
+<script type="text/javascript" src="http://ajax.googleapis.com/ajax/libs/jquery/1.10.1/jquery.min.js"></script>
+
+<script type="text/javascript">
+
+var check_array = [];
+
+$(document).ready(function(){
+
+	$(".fields").click(function(){
+	
+		dataval = $(this).data('val');
+	
+		// Show the Selected Slots box if someone selects a slot
+		if($("#outer_basket").css("display") == 'none') { 
+			$("#outer_basket").css("display", "block");
+		}
+
+		if(jQuery.inArray(dataval, check_array) == -1) {
+			check_array.push(dataval);
+		} else {
+			// Remove clicked value from the array
+			check_array.splice($.inArray(dataval, check_array) ,1);	
+		}
+		
+		slots=''; hidden=''; basket = 0;
+		
+		cost_per_slot = $("#cost_per_slot").val();
+		//cost_per_slot = parseFloat(cost_per_slot).toFixed(2)
+
+		for (i=0; i< check_array.length; i++) {
+			slots += check_array[i] + '\r\n';
+			hidden += check_array[i].substring(0, 8) + '|';
+			basket = (basket + parseFloat(cost_per_slot));
+		}
+		
+		// Populate the Selected Slots section
+		$("#selected_slots").html(slots);
+		
+		// Update hidden slots_booked form element with booked slots
+		$("#slots_booked").val(hidden);		
+
+		// Update basket total box
+		basket = basket.toFixed(2);
+		$("#total").html(basket);	
+
+		// Hide the basket section if a user un-checks all the slots
+		if(check_array.length == 0)
+		$("#outer_basket").css("display", "none");
+		
+	});
+	
+	
+	$(".classname").click(function(){
+	
+		msg = '';
+	
+		if($("#name").val() == '')
+		msg += 'Please enter a Name\r\n';
+
+		if($("#email").val() == '')
+		msg += 'Please enter an Email address\r\n';
+
+		if($("#phone").val() == '')
+		msg += 'Please enter a Phone number\r\n';	
+
+		if(msg != '') {
+			alert(msg);
+			return false;
+		}
+
+	});
+
+	// Firefox caches the checkbox state.  This resets all checkboxes on each page load 
+	$('input:checkbox').removeAttr('checked');
+	
+});
+
+
+
+
+</script>
+
+</head>
+<body>
+
+<?php     
+        
+if($_SERVER['REQUEST_METHOD'] == 'POST') {
+    $calendar->after_post($month, $day, $year);  
+}   
+
+// Call calendar function
+$calendar->make_calendar($selected_date, $back, $forward, $day, $month, $year);
+
+?>
+
+</body>
+</html>
---
+++ docroot/schedule/calendar_table_schema.sql
@@ -0,0 +1,41 @@
+-- phpMyAdmin SQL Dump
+-- version 3.5.2.2
+-- http://www.phpmyadmin.net
+--
+-- Host: 127.0.0.1
+-- Generation Time: May 24, 2014 at 09:13 AM
+-- Server version: 5.5.27
+-- PHP Version: 5.4.7
+
+SET SQL_MODE="NO_AUTO_VALUE_ON_ZERO";
+SET time_zone = "+00:00";
+
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8 */;
+
+--
+-- Database: `planephp_bookings`
+--
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `bookings`
+--
+
+CREATE TABLE IF NOT EXISTS `bookings` (
+  `id` int(100) NOT NULL AUTO_INCREMENT,
+  `date` date NOT NULL,
+  `start` time NOT NULL,
+  `name` varchar(100) COLLATE utf8_unicode_ci NOT NULL,
+  `email` varchar(200) COLLATE utf8_unicode_ci NOT NULL,
+  `phone` varchar(50) COLLATE utf8_unicode_ci NOT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=MyISAM DEFAULT CHARSET=utf8 COLLATE=utf8_unicode_ci AUTO_INCREMENT=1 ;
+
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
