6f898a890c39a259d89ad8b45e971f375a64d68d
Sean Hanford <sean.hanford@ellucian.com>
2016-11-10 14:29:03 -0600

    Updated NPC and created duplicate of wetkit_bootstrap called tiffin_bootstrap, but it is not ready for production use yet.

---
+++ docroot/profiles/wetkit/themes/tiffin_bootstrap/js/animated-menu-line.js
@@ -0,0 +1,5 @@
+
+jQuery(document).ready(function($) {
+    $('ul.list-inline li a:first-child').addClass('hvr-underline-reveal');
+    $('ul.list-inline li ul.list-unstyled li a').removeClass('hvr-underline-reveal');
+});
