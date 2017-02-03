5583a5246995dfe13c21f361c98e61afaabf47af
LaTrelle Freeman <latrelle.freeman@ellucian.com>
2017-02-03 14:37:33 -0500

    update related to container class issue

--- docroot/profiles/wetkit/themes/wetkit_bootstrap/js/miscTiffinUpdates.js
+++ docroot/profiles/wetkit/themes/wetkit_bootstrap/js/miscTiffinUpdates.js
@@ -8,7 +8,10 @@
         {
             //console.log("international app init");
             jQuery('main').attr('class', 'container');
+            jQuery('footer > div').first().removeClass("container");
+            jQuery('footer > div').first().addClass("container-fluid");
+            jQuery('nav#wb-sm > div').removeClass("container");
 
         }//attach:function
-    };
+    }
 }(jQuery, Drupal));
