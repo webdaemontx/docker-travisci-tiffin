a7fb93b7c89bcc2af5db739d5b1d7e8c264ad223
Sean Hanford <sean.hanford@ellucian.com>
2017-03-16 15:13:39 -0500

    Find My Program Form - Updated
    Added another Panels Page /academics/all-programs to display if neither Graduate or Undergraduate or Select an Interest has been chosen, they will be redirected to this page that displays All Academic Programs.

--- docroot/profiles/wetkit/themes/wetkit_bootstrap/js/frontpage.js
+++ docroot/profiles/wetkit/themes/wetkit_bootstrap/js/frontpage.js
@@ -9,23 +9,26 @@
 
 
                 if (jQuery("input[id=edit-submitted-degree-type-1]:checked", "#webform-uuid-3e910914-01b0-4ad1-8d70-7f6116c073d7").val() == "undergraduate") {
-                    if (jQuery("#edit-submitted-graduate").val() == null) {
-                        jQuery(location).attr( 'href', '/academics/undergrad' );
-                    } else if (jQuery("#edit-submitted-undergraduate").val() !== null) {
+
+                    if (jQuery("#edit-submitted-undergraduate").val() > 0) {
                         var selectedProgram = jQuery("#edit-submitted-undergraduate").val();
                         jQuery(location).attr( 'href', '/node/' + selectedProgram );
+                    } else {
+                        jQuery(location).attr( 'href', '/academics/undergrad' );
                     }
                     return false;
 
                 } else if (jQuery("input[id=edit-submitted-degree-type-2]:checked", "#webform-uuid-3e910914-01b0-4ad1-8d70-7f6116c073d7").val() == "graduate") {
-                    if (jQuery("#edit-submitted-graduate").val() == null) {
-                        jQuery(location).attr('href', '/academics/graduate');
-                    } else if (jQuery("#edit-submitted-graduate").val() !== null) {
+
+                    if (jQuery("#edit-submitted-graduate").val() > 0) {
                         var selectedProgram = jQuery("#edit-submitted-graduate").val();
                         jQuery(location).attr( 'href', '/node/' + selectedProgram );
+                    } else {
+                        jQuery(location).attr('href', '/academics/graduate');
                     }
                     return false;
                 }
+                //event.preventDefault();
             }); //submit(event)
 
         } //attach:function
