8cb4257227edeb6965740b985fc442372c2d467b
Sean Hanford <sean.hanford@ellucian.com>
2017-03-16 16:16:46 -0500

    Find My Program Form - Updated
    Completed the logic-tree for each possible submit on the form.

--- docroot/profiles/wetkit/themes/wetkit_bootstrap/js/frontpage.js
+++ docroot/profiles/wetkit/themes/wetkit_bootstrap/js/frontpage.js
@@ -13,22 +13,29 @@
                     if (jQuery("#edit-submitted-undergraduate").val() > 0) {
                         var selectedProgram = jQuery("#edit-submitted-undergraduate").val();
                         jQuery(location).attr( 'href', '/node/' + selectedProgram );
+                        console.log("redirecting to some undergrad program");
                     } else {
                         jQuery(location).attr( 'href', '/academics/undergrad' );
+                        console.log("redirecting to undergrad");
                     }
-                    return false;
-
                 } else if (jQuery("input[id=edit-submitted-degree-type-2]:checked", "#webform-uuid-3e910914-01b0-4ad1-8d70-7f6116c073d7").val() == "graduate") {
 
                     if (jQuery("#edit-submitted-graduate").val() > 0) {
                         var selectedProgram = jQuery("#edit-submitted-graduate").val();
                         jQuery(location).attr( 'href', '/node/' + selectedProgram );
+                        console.log("redirecting to some grad program");
                     } else {
                         jQuery(location).attr('href', '/academics/graduate');
+                        console.log("redirecting to graduate");
                     }
-                    return false;
+                } else if (jQuery("#edit-submitted-placeholder").val() > 0) {
+                    var selectedProgram = jQuery("#edit-submitted-placeholder").val();
+                    jQuery(location).attr('href', '/node/' + selectedProgram);
+                } else {
+                    jQuery(location).attr('href', '/academics/all-programs');
+                    console.log("redirecting to all-programs");
                 }
-                //event.preventDefault();
+                event.preventDefault();
             }); //submit(event)
 
         } //attach:function
