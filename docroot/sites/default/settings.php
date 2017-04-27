0aaf5068f6b90001af5cde2e3cf079883625a120
Sean Hanford <sean.hanford@ellucian.com>
2017-04-27 13:58:32 -0500

    Settings.php update for production

--- docroot/sites/default/settings.php
+++ docroot/sites/default/settings.php
@@ -360,7 +360,7 @@ ini_set('session.cookie_lifetime', 2000000);
  * between your various domains. Make sure to always start the $cookie_domain
  * with a leading dot, as per RFC 2109.
  */
- $cookie_domain = '.tiffin.edu';
+ #$cookie_domain = '.tiffin.edu';
 
 /**
  * Variable overrides:
