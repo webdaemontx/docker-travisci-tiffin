d02083b03cb6b8e03b42fae9bd2f176043830bea
Sean Hanford <sean.hanford@ellucian.com>
2017-08-08 13:58:35 -0500

    Added Online Option to homepage Find My Program form
    JS alteration to frontpage.js to change the default text for the select.
    CSS update to _base.scss to allow three radio options to be displayed in-line.

--- docroot/profiles/wetkit/themes/wetkit_bootstrap/js/frontpage.js
+++ docroot/profiles/wetkit/themes/wetkit_bootstrap/js/frontpage.js
@@ -1,10 +1,16 @@
 /**
- * Created by shanford on 3/15/17.
+ * Updated by shanford on 8/3/17.
  */
 (function($, Drupal) {
   Drupal.behaviors.frontpage = {
     attach: function()
     {
+      jQuery('#edit-submitted-online option').each(function() {
+        if (jQuery(this).text() === '- None -') {
+          jQuery(this).text('Select an interest');
+        }
+      });
+
       jQuery('#webform-uuid-3e910914-01b0-4ad1-8d70-7f6116c073d7').submit(function(event) {
 
         if (jQuery('input[id=edit-submitted-degree-type-1]:checked', '#webform-uuid-3e910914-01b0-4ad1-8d70-7f6116c073d7').val() === 'undergraduate') {
@@ -12,21 +18,31 @@
             if (jQuery('#edit-submitted-undergraduate').val() > 0) {
               var selectedProgram = jQuery('#edit-submitted-undergraduate').val();
               jQuery(location).attr('href', '/node/' + selectedProgram);
-              console.log('redirecting to some undergrad program');
+              console.log('redirecting to an undergrad program');
             } else {
               jQuery(location).attr('href', '/academics/undergrad');
-              console.log('redirecting to undergrad');
+              console.log('redirecting to undergrad landing page');
             }
         } else if (jQuery('input[id=edit-submitted-degree-type-2]:checked', '#webform-uuid-3e910914-01b0-4ad1-8d70-7f6116c073d7').val() === 'graduate') {
 
             if (jQuery("#edit-submitted-graduate").val() > 0) {
               var selectedProgram = jQuery("#edit-submitted-graduate").val();
               jQuery(location).attr('href', '/node/' + selectedProgram);
-              console.log('redirecting to some grad program');
+              console.log('redirecting to a grad program');
             } else {
               jQuery(location).attr('href', '/academics/graduate');
-              console.log('redirecting to graduate');
+              console.log('redirecting to graduate landing page');
             }
+        } else if (jQuery('input[id=edit-submitted-degree-type-3]:checked', '#webform-uuid-3e910914-01b0-4ad1-8d70-7f6116c073d7').val() === 'online') {
+
+          if (jQuery("#edit-submitted-online").val() > 0) {
+            var selectedProgram = jQuery("#edit-submitted-online").val();
+            jQuery(location).attr('href', '/node/' + selectedProgram);
+            console.log('redirecting to an online program');
+          } else {
+            jQuery(location).attr('href', '/degreecomp/welcome');
+            console.log('redirecting to online landing page');
+          }
         } else if (jQuery('#edit-submitted-placeholder').val() > 0) {
           var selectedProgram = jQuery('#edit-submitted-placeholder').val();
           jQuery(location).attr('href', '/node/' + selectedProgram);
--- docroot/profiles/wetkit/themes/wetkit_bootstrap/js/pictureu.js
+++ docroot/profiles/wetkit/themes/wetkit_bootstrap/js/pictureu.js
@@ -1,26 +1,16 @@
-/*
- * Copyright (c) 2017. Lorem ipsum dolor sit amet, consectetur adipiscing elit.
- * Morbi non lorem porttitor neque feugiat blandit. Ut vitae ipsum eget quam lacinia accumsan.
- * Etiam sed turpis ac ipsum condimentum fringilla. Maecenas magna.
- * Proin dapibus sapien vel ante. Aliquam erat volutpat. Pellentesque sagittis ligula eget metus.
- * Vestibulum commodo. Ut rhoncus gravida arcu.
- */
-
-
-
 
 (function($, Drupal)
 {
   Drupal.behaviors.pictureu = {
-    attach:function()
+    attach: function()
     {
       //console.log("international app init");
-      jQuery("#edit-submitted-select-program-of-interest option").each(function() {
+      jQuery('#edit-submitted-select-program-of-interest option').each(function() {
         if (jQuery(this).text() === '- Select -') {
           jQuery(this).text('- Select Program of Interest -');
         }
     });
 
     }//attach:function
-  }
+  };
 }(jQuery, Drupal));
