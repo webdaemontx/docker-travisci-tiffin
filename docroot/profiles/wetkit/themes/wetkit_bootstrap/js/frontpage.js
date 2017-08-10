b99f5fc86cdbbef75aa7c5d3b5faf26b870f51e5
Sean Hanford <sean.hanford@ellucian.com>
2017-08-09 22:47:22 -0500

    Homepage Find My Program CSS Styling - Responsive

--- docroot/profiles/wetkit/themes/wetkit_bootstrap/js/frontpage.js
+++ docroot/profiles/wetkit/themes/wetkit_bootstrap/js/frontpage.js
@@ -13,7 +13,8 @@
 
       jQuery('#webform-uuid-3e910914-01b0-4ad1-8d70-7f6116c073d7').submit(function(event) {
 
-        if (jQuery('input[id=edit-submitted-degree-type-1]:checked', '#webform-uuid-3e910914-01b0-4ad1-8d70-7f6116c073d7').val() === 'undergraduate') {
+        if (jQuery('input[id=edit-submitted-degree-type-1]:checked',
+             '#webform-uuid-3e910914-01b0-4ad1-8d70-7f6116c073d7').val() === 'undergraduate') {
 
             if (jQuery('#edit-submitted-undergraduate').val() > 0) {
               var selectedProgram = jQuery('#edit-submitted-undergraduate').val();
@@ -23,19 +24,21 @@
               jQuery(location).attr('href', '/academics/undergrad');
               console.log('redirecting to undergrad landing page');
             }
-        } else if (jQuery('input[id=edit-submitted-degree-type-2]:checked', '#webform-uuid-3e910914-01b0-4ad1-8d70-7f6116c073d7').val() === 'graduate') {
+        } else if (jQuery('input[id=edit-submitted-degree-type-2]:checked',
+              '#webform-uuid-3e910914-01b0-4ad1-8d70-7f6116c073d7').val() === 'graduate') {
 
-            if (jQuery("#edit-submitted-graduate").val() > 0) {
-              var selectedProgram = jQuery("#edit-submitted-graduate").val();
+            if (jQuery('#edit-submitted-graduate').val() > 0) {
+              var selectedProgram = jQuery('#edit-submitted-graduate').val();
               jQuery(location).attr('href', '/node/' + selectedProgram);
               console.log('redirecting to a grad program');
             } else {
               jQuery(location).attr('href', '/academics/graduate');
               console.log('redirecting to graduate landing page');
             }
-        } else if (jQuery('input[id=edit-submitted-degree-type-3]:checked', '#webform-uuid-3e910914-01b0-4ad1-8d70-7f6116c073d7').val() === 'online') {
+        } else if (jQuery('input[id=edit-submitted-degree-type-3]:checked',
+              '#webform-uuid-3e910914-01b0-4ad1-8d70-7f6116c073d7').val() === 'online') {
 
-          if (jQuery("#edit-submitted-online").val() > 0) {
+          if (jQuery('#edit-submitted-online').val() > 0) {
             var selectedProgram = jQuery("#edit-submitted-online").val();
             jQuery(location).attr('href', '/node/' + selectedProgram);
             console.log('redirecting to an online program');
